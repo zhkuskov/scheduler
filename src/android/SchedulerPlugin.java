c5dfbcb46e7e8b70b1a52bfabcc1c20e5625810b
Евгений <me.kuskov@gmail.com>
2017-01-29 21:33:12 +0300

    Add files via upload

---
+++ src/android/SchedulerPlugin.java
@@ -0,0 +1,61 @@
+package com.example.android.scheduler;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+
+import android.util.Log;
+
+import org.apache.cordova.CordovaPlugin;
+
+/*import com.red_folder.phonegap.plugin.backgroundservice.BackgroundServicePluginLogic.ExecuteResult;
+import com.red_folder.phonegap.plugin.backgroundservice.BackgroundServicePluginLogic.ExecuteStatus;*/
+
+public class SchedulerPlugin extends CordovaPlugin{
+
+	/*
+	 ************************************************************************************************
+	 * Static values 
+	 ************************************************************************************************
+	 */
+	private static final String TAG = BackgroundServicePlugin.class.getSimpleName();
+
+SampleAlarmReceiver alarm = new SampleAlarmReceiver();
+
+/*-------------*/
+   @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+
+        if (action.equals("start")) {
+
+            /*
+cordova.getActivity().runOnUiThread(new Runnable() {
+                public void run() {
+                    webView.postMessage("SchedulerPlugin", "start");
+                }
+            });
+*/
+
+Context context=cordova.getActivity().getApplicationContext();
+
+alarm.setAlarm(context);
+
+        } else if (action.equals("stop")) {
+            /*cordova.getActivity().runOnUiThread(new Runnable() {
+                public void run() {
+                    webView.postMessage("splashscreen", "show");
+                }
+            });
+*/
+        } else {
+            return false;
+        }
+
+        callbackContext.success();
+        return true;
+    }
+
+
+
+
+}
