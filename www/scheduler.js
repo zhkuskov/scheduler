a6b13e2a11f9ea70bcb850caff3738984856578f
Евгений <me.kuskov@gmail.com>
2017-01-29 21:32:28 +0300

    Add files via upload

--- www/scheduler.js
+++ www/scheduler.js
@@ -1,1 +1,12 @@
+var exec = require('cordova/exec');
 
+var scheduler = {
+    start:function() {
+        exec(null, null, "SchedulerPlugin", "start", []);
+    },
+    stop:function() {
+        exec(null, null, "SchedulerPlugin", "stop", []);
+    }
+};
+
+module.exports = scheduler;
