edbe02c3b4b435d5e9d3b7cff53ebbc17cea932f
Евгений <me.kuskov@gmail.com>
2017-01-30 15:34:12 +0300

    Add files via upload

--- src/android/SchedulerPlugin.java
+++ src/android/SchedulerPlugin.java
@@ -4,11 +4,7 @@ import org.json.JSONArray;
 
 import android.util.Log;
 
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaInterface;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.PluginResult;
+import org.apache.cordova.*;
 
 //import android.content.Context;
 
